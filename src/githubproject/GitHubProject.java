e10fa4c6ecf9107ace245dad7eaad1c3d5620c31
Megha Mapalagama <meghamapalagama@meghas-mbp.home>
2020-11-07 15:38:56 -0500

    helper method

--- src/githubproject/GitHubProject.java
+++ src/githubproject/GitHubProject.java
@@ -3,6 +3,9 @@ package githubproject;
 public class GitHubProject {
 
     public static void main(String[] args) {
-        System.out.println("Welcom to Hello World !  ");
+        System.out.println("Welcome to Hello World !  ");
+    }
+    public GitHubProject(){
+        System.out.println("My work");
     }
 }
